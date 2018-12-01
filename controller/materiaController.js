10a28e03f2948950687116a21a402b9e17f413be
kaztro <00229017@uca.edu.sv>
2018-12-01 15:41:35 -0600

    Se termino el controlador getAll y getOne

--- controller/materiaController.js
+++ controller/materiaController.js
@@ -3,18 +3,56 @@ var materiaController = {};
 var materiaModel = import('../models/Materia');
 
 materiaController.getAll = function (req, res) {
-    console.log('random.text');
+    materiaModel.find({}, function(err, materias) {
+        if (err) {
+            res.json({
+                code: 500,
+                success: false,
+                err
+            });
+        } else {
+            res.json({
+                code: 200,
+                success: true,
+            })
+            res.json(materias);
+        }
+    });
 };
 
 materiaController.getOne = function (req, res) {
-    console.log('random.text');
+    materiaModel.find({ _id: req.params.id }, function(err, materia) {
+        if (err) {
+            res.json({
+                code: 500,
+                success: false,
+                err
+            });
+        } else {
+            res.json({
+                code: 200,
+                success: true,
+            })
+            res.json(materia);
+        }
+    });
 };
 materiaController.store = function (req, res) {
     console.log('random.text');
 };
 
 materiaController.update = function (req, res) {
-    console.log('random.text');
+    materiaModel.findByIdAndUpdate({ _id: req.params.id }, function(err, materia) {
+        if (err) {
+            res.json({
+                code: 500,
+                success: false,
+                err
+            });
+        } else {
+            res.json(materia);
+        }
+    });
 };
 
 materiaController.delete = function (req, res) {
